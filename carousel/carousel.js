4dd585d1d499c706662cffeabdbf22dc25156f6b
optimisticlucio <73440828+optimisticlucio@users.noreply.github.com>
2025-06-11 11:55:44 +0300

    Functional carousel

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,19 +1,32 @@
 // Goes over all items with the carousel class and turns them to proper carousels.
 function generateCarousels() {
-    const carousels = document.querySelectorAll('.carousel');
+    const carousels = Array.from(document.querySelectorAll('.carousel'));
 
     for (let carousel of carousels) {
         const carouselTitle = document.createElement("h1");
         carouselTitle.classList.add("carousel-title");
         carouselTitle.innerText = carousel.dataset.title ?? "Carousel Title Missing";
 
-        const carouselContentHolder = generateCarouselContentHolder(carousel.children)
+        const carouselContentHolder = generateCarouselContentHolder(Array.from(carousel.children));
+        const carouselLeftButton = document.createElement("button");
+        carouselLeftButton.classList.add("carousel-button");
+        carouselLeftButton.addEventListener("click", (e) => {
+            moveCarouselToLeft(carousel, carouselContentHolder);
+        });
+        const carouselRightButton = document.createElement("button");
+        carouselRightButton.classList.add("carousel-button");
+        carouselRightButton.addEventListener("click", (e) => {
+            moveCarouselToRight(carousel, carouselContentHolder);
+        });
 
         const carouselContentAndButtons = document.createElement("div");
         carouselContentAndButtons.classList.add("carousel-content-and-buttons");
-        carouselContentAndButtons.append(carouselContentHolder);
+        carouselContentAndButtons.append(carouselLeftButton, carouselContentHolder, carouselRightButton);
 
-        // TODO: Add dots
+        // TODO: Implement generateCarouselDotHolder and connect
+
+        carousel.dataset.currentlyDisplaying = "0";
+        carouselContentHolder.children[0].classList.remove("hidden");
 
         carousel.append(carouselTitle, carouselContentAndButtons)
     }
@@ -32,5 +45,24 @@ function generateCarouselContentHolder(carouselContents) {
     return carouselContentHolder;
 }
 
+function generateCarouselDotHolder(amountOfDots) {
+    // TODO: implement
+}
 
-generateCarousels();
+function moveCarouselToRight(carousel, carouselContentHolder) {
+    const carouselChildren = carouselContentHolder.children;
+    carouselChildren[carousel.dataset.currentlyDisplaying].classList.add("hidden");
+    carousel.dataset.currentlyDisplaying = ((parseInt(carousel.dataset.currentlyDisplaying) + 1 + carouselChildren.length) % carouselChildren.length).toString();
+    carouselChildren[carousel.dataset.currentlyDisplaying].classList.remove("hidden");
+}
+
+function moveCarouselToLeft(carousel, carouselContentHolder) {
+    const carouselChildren = carouselContentHolder.children;
+    carouselChildren[carousel.dataset.currentlyDisplaying].classList.add("hidden");
+    carousel.dataset.currentlyDisplaying = ((parseInt(carousel.dataset.currentlyDisplaying) - 1 + carouselChildren.length) % carouselChildren.length).toString();
+    carouselChildren[carousel.dataset.currentlyDisplaying].classList.remove("hidden");
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    generateCarousels();
+});
