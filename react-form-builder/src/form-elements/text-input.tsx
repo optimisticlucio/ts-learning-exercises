20d4152bf6ab63ca4f552bb8a9a43a028302352f
optimisticlucio <73440828+optimisticlucio@users.noreply.github.com>
2025-08-18 11:15:17 +0300

    Implemented text input

--- react-form-builder/src/form-elements/text-input.tsx
+++ react-form-builder/src/form-elements/text-input.tsx
@@ -5,7 +5,20 @@ export default function TextInput({
                                   }: {
     settings: TextInput;
 }) {
-    // TODO
-
-    return <></>;
+    return <>
+        <label id={settings.id}>
+            {settings.title}
+        </label>
+        <br />
+        {() => {
+            // There's a lot of overlap here. I could have done {variant == "short" ? input type=text : textarea},
+            // but that seems less readable and more prone to becoming a mess if more variants need to be added.
+            switch (settings.variant) {
+                case "short":
+                    return (<input type="text" name={settings.id} id={settings.id} value={settings.defaultValue}/>);
+                case "area":
+                    return (<textarea name={settings.id} id={settings.id} value={settings.defaultValue} />);
+            }
+        }}
+    </>;
 }
